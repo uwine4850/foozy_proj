f8b40a6c64906c5eb1a9b35c00cc986f8179de49
uwine4850 <uwine4850@gmail.com>
2023-10-22 15:45:05 +0300

    A template for a profile page.

--- src/main.go
+++ src/main.go
@@ -23,6 +23,13 @@ func main() {
 			panic(err)
 		}
 	})
+	newRouter.Get("/profile", func(w http.ResponseWriter, r *http.Request, manager interfaces.IManager) {
+		manager.SetTemplatePath("src/templates/profile.html")
+		err := manager.RenderTemplate(w, r)
+		if err != nil {
+			panic(err)
+		}
+	})
 	newRouter.GetMux().Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("src/static"))))
 	server := server2.NewServer(":8000", newRouter)
 	err = server.Start()
--- src/static/js/index.ts
+++ src/static/js/index.ts
@@ -6,3 +6,9 @@ runIfExist(document.getElementById("header-user"), function (el) {
         document.getElementById("hu-menu").classList.toggle("hu-menu-hidden");
     }
 });
+
+runIfExist(document.getElementById("pc-menu-filter-value"), function (el){
+   el.onclick = function (){
+        document.getElementById("filter-items").classList.toggle("filter-items-hidden");
+   }
+});
