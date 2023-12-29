bad336bdd55ed7d6b19736f49c00296d62c2c2a0
uwine4850 <uwine4850@gmail.com>
2023-12-29 15:06:54 +0200

    Added typescript class for displaying popups.

--- src/static/js/index.ts
+++ src/static/js/index.ts
@@ -5,12 +5,19 @@ import {RunWs} from "./chat_ws";
 import {runLazyLoadMsg, runLazyLoadNotReadMsg} from "./lazy_load_msg";
 import {RunWsNotification} from "./notification_ws";
 import {OnImagesSelect, SendAjaxChatMessage} from "./chat/chat";
+import {PopUp} from "./pop_up";
 
 runIfExist(document.getElementById("pp-del-avatar-label"), function (el){
    el.onclick = function (){
        document.getElementById("pp-del-avatar").classList.toggle("page-panel-checkbox-true");
    }
 });
+
+let popUp = new PopUp("pop-up-panel", true);
+popUp.onClick(function (popUp){
+   popUp.classList.toggle("pop-up-hide");
+});
+popUp.start();
 
 const regex = /^\/chat\/\d+$/;
 RunWsNotification();
---
+++ src/static/js/pop_up.ts
@@ -0,0 +1,53 @@
+// <div class="pop-up-panel-bg">
+//     <div class="pop-up-panel">
+//         <button class="pop-up-activate">Activate</button>
+//         <div class="pop-up-content pop-up-hide">
+//             Text
+//         </div>
+//     </div>
+//     <div class="pop-up-bg pop-up-bg-hide"></div>
+// </div>
+
+export class PopUp {
+    private readonly className: string;
+    private readonly isBackground: boolean;
+    private _onClick: (popUp: HTMLElement) => void;
+    constructor(className: string, isBackground: boolean) {
+        this.className = className;
+        this.isBackground = isBackground;
+    }
+
+    public onClick(click: (popUp: HTMLElement) => void){
+        this._onClick = click;
+    }
+
+    private processingBg(pop_up_bg: HTMLElement){
+        pop_up_bg.onclick = function (){
+            let parent = pop_up_bg.parentElement;
+            let panel = parent.getElementsByClassName("pop-up-panel")[0] as HTMLElement;
+            let button = panel.getElementsByClassName("pop-up-activate")[0] as HTMLButtonElement;
+            button.click();
+        }
+    }
+
+    public start(){
+        let popUps = Array.from(document.getElementsByClassName(this.className)) as HTMLElement[];
+        for (let i = 0; i < popUps.length; i++) {
+            let pop_up_bg: HTMLElement;
+            if (this.isBackground){
+                let pop_up_panel_bg = popUps[i].parentElement;
+                pop_up_bg = pop_up_panel_bg.getElementsByClassName("pop-up-bg")[0] as HTMLElement;
+            }
+            let popUpActivate = popUps[i].getElementsByClassName("pop-up-activate");
+            popUpActivate[0].addEventListener("click",  () => {
+                let s: HTMLElement = popUps[i].getElementsByClassName("pop-up-content")[0] as HTMLElement
+                if (pop_up_bg){
+                    this.processingBg(pop_up_bg);
+                    pop_up_bg.classList.toggle("pop-up-bg-hide");
+                }
+                this._onClick(s);
+            })
+        }
+    }
+}
+
